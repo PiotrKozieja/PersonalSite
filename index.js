bbaf9ede6862b8725d13c6dfaef8105646f94c79
PiotrKozieja <piotrusiekmaster@gmail.com>
2024-02-05 12:10:15 +0100

    All files added

---
+++ index.js
@@ -0,0 +1,91 @@
+function OPW(liczba) {
+    if (liczba >= 0) {
+        var wysokoscEkranu = window.innerHeight;
+        var procentWysokosci = (liczba / 100) * wysokoscEkranu;
+        return procentWysokosci;
+    }
+}
+console.log(OPW(50))
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#top",{
+    scrollTrigger:{
+        toggleActions: "restart pause reverse pause",
+        scrub:true,
+        trigger:"#top",
+        start: "top -" + OPW(40) + "px",
+        end: "top -" + OPW(70) + "px",
+    },
+    y:"-10vh",
+    duration: 5,
+    ease: "linear"
+})
+gsap.to("#decor-container",{
+    scrollTrigger:{
+        toggleActions: "restart pause reverse pause",
+        scrub:true,
+        trigger:"#top",
+        start: "top -" + OPW(10) + "px",
+        end: "top -" + OPW(50) + "px",
+    },
+    y:"-30vh",
+    duration: 10,
+    ease: "linear"
+})
+gsap.to("#buttons",{
+    scrollTrigger:{
+        toggleActions: "restart pause reverse pause",
+        scrub:true,
+        trigger:"#top",
+        start: "top -" + OPW(10) + "px",
+        end: "top -" + OPW(50) + "px",
+
+    },
+    x:"25vw",
+    duration: 10,
+    ease: "power1.inOut"
+})
+gsap.to("#txt1",{
+    scrollTrigger:{
+        toggleActions: "restart pause reverse pause",
+        scrub:true,
+        trigger:"#top",
+        start: "top -" + OPW(10) + "px",
+        end: "top -" + OPW(50) + "px",
+
+    },
+    x:"-25vw",
+    duration: 5,
+    ease: "power1.inOut"
+})
+gsap.to("#rwai",{
+    scrollTrigger:{
+        toggleActions: "restart pause reverse pause",
+        scrub:true,
+        trigger:"#top",
+        start: "top -" + OPW(30) + "px",
+        end: "top -" + OPW(62) + "px",
+    },
+    x:"23vw",
+    duration: 10,
+    ease: "linear"
+})
+gsap.to("#lwai",{
+    scrollTrigger:{
+        toggleActions: "restart pause reverse pause",
+        scrub:true,
+        trigger:"#top",
+        start: "top -" + OPW(30) + "vh",
+        end: "top -" + OPW(62) + "vh",
+    },
+    x:"-20vw",
+    duration: 10,
+    ease: "linear"
+})
+ScrollTrigger.create({
+    trigger: "#img1",
+    start:"top top",
+    end:"+="+ OPW(55) + "vh",
+    pin: true,
+    pinSpacing: false
+})
+
